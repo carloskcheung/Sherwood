ab354ede8a586aa1d3eca40471e3296bebc579f9
Carlos Cheung <cc653@georgetown.edu>
2013-09-12 16:02:05 -0700

    Initialize Rails Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sherwood::Application.config.secret_key_base = '6bc3a0d1807a0511ddb15bca5655d459b0a3ad6ba94cb1faa8ea75e37009ba70baf0b679bd8338bd8436b55a036412c2072e558546b8cafb6cd7578df1d341d3'
